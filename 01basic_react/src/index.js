ccb4e0505cb50c15eb25b61082e64accde4c619d
AnshumanSachdeva <anshumansachdeva8@gmail.com>
2026-02-17 20:46:52 +0530

    Comment

--- 01basic_react/src/index.js
+++ 01basic_react/src/index.js
@@ -9,3 +9,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// html file k andar humne script toh kahi par nhi di fir yeh js ka code chal kaise raha hai??
+// humare pass package.json k andar dependencies k andar ek cheej hoti hai "react-script" yehi index.js ko html file k sath link kar leti hai, Yeh cheej hum check kar skte hai inspect karke

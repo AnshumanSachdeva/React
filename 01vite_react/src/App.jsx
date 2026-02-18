2c686384bdc595fa0d0629042dce2ae0567d4b3f
AnshumanSachdeva <anshumansachdeva8@gmail.com>
2026-02-18 20:58:40 +0530

    Checked and understand why mine element was not working, then done using React.createElement

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from "./chai.jsx"
 
 function App() {
-  
+  const username = "anshuman"
 
   return (
     <>
-      <h1>React with vite</h1>
+      <h1>React with vite | {username}yeh jo curly braces mein kiya hai isko evaluated expression bolte hai, yeh js nhi hai uska result aarha hai hum ismein direct js nhi likh skte</h1>
       <Chai/>
+      // yeh jo curly braces mein kiya hai isko evaluated expression bolte hai, yeh js nhi hai uska result aarha hai hum ismein direct js nhi likh skte
     </>
   )
 }
--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -1,9 +1,34 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// }
+
+const anotherEle = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactEle = React.createElement(
+  'a',
+  {href: 'https://google.com',target:'_blank'},
+  'click to visit google',
+  anotherUser
+)
+// jo syntax upr use kiya hai vo humne actual react ka use karke banaya hai isliye vo valid hai also , is syntax mein humein proper series mein hi cheeje deni padhti hai .ie. 1. tag, 2. object, 3. text, 4. var injection (Evaluated expression)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    // <App />
+    // reactElement   // yeh q nhi chala?? qki yeh syntax sahi nhi hai because yeh jisne b vite se bnaya hai uske b kush prerequisites hai toh usne yeh bola hai ki aise tarike se hi karna hai jaise niche wala chalaya
+    // anotherEle // this will work
+    // reactEle  // this will also work
 )
